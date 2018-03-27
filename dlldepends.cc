a248fad1bb5ea38a37f4ad536771e822c6094a1c
JP Cimalando <jpcima@users.noreply.github.com>
2018-03-27 02:59:18 +0200

    add license

--- dlldepends.cc
+++ dlldepends.cc
@@ -1,3 +1,7 @@
+//          Copyright Jean Pierre Cimalando 2018.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
 
 #include <llvm/Object/COFF.h>
 #include <getopt.h>
